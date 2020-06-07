901bb0ba0e133a57959d6976dc215203183ce54e
Wataru Kume <kumewata@gmail.com>
2020-06-07 10:00:48 +0900

    initial commit

---
+++ lib/rspec_creative_traveler.rb
@@ -0,0 +1,6 @@
+require "rspec_creative_traveler/version"
+
+module RspecCreativeTraveler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rspec_creative_traveler/version.rb
@@ -0,0 +1,3 @@
+module RspecCreativeTraveler
+  VERSION = "0.1.0"
+end
